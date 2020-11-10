2dc36ab7fd7bc90b93a25f6c8b2201a025d745b4
winebarrel <sugawara@winebarrel.jp>
2020-11-10 11:20:33 +0900

    Use context.WithCancel()

--- task.go
+++ task.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/siddontang/go-log/log"
+
 	"golang.org/x/sync/errgroup"
 )
 
@@ -36,15 +38,31 @@ func (task *Task) Run(dryrun bool) error {
 	}
 
 	evch := make(chan Event)
-	eg, ctx := errgroup.WithContext(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
+	eg, etCtx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error {
-		return replica.Repeat(evch, ctx)
+		defer func() {
+			cancel()
+
+			// Skip events
+			for ev := range evch {
+				_ = ev
+			}
+		}()
+
+		err := replica.Repeat(evch, etCtx)
+
+		if err != nil {
+			log.Errorf("Faild to repeat binlog: %s", err)
+		}
+
+		return err
 	})
 
 	eg.Go(func() error {
 		defer close(evch)
-		return binlog.Receive(evch, ctx)
+		return binlog.Receive(evch, etCtx)
 	})
 
 	return eg.Wait()
